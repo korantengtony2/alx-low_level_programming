26c5bc2bf783810c845037509777825528dc454b
korantengtony2 <korantengtony2@gmail.com>
2022-12-20 09:27:42 -0800

    reset

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _ispper - checks if uppercase
+ * @c: character to check
+ *
+ * Return: gives 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && v <= 'Z')
+
+	{
+		return (1);
+	}
+	return (0);
+}
