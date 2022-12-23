0bde7764c02b7d90d2fbd35ae411966f9100399b
korantengtony2 <korantengtony2@gmail.com>
2022-12-23 08:15:57 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
 	return (dest);
 }
