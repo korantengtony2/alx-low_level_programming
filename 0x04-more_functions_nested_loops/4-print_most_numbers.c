e1d72f5b0c724d3d56756044149920b0dceb5961
korantengtony2 <korantengtony2@gmail.com>
2023-01-04 13:11:41 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers
+ * print_most_numbers - prints the numbers
  *
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i);
+		if (i != '2' &&  i != '4')
+		{
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
 }
