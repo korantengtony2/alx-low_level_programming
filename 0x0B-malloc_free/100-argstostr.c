64687cc1399efefe33fe5e48a7dfee710fa8f191
korantengtony2 <korantengtony2@gmail.com>
2023-01-11 13:59:43 -0800

    task 6

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,60 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * argstostr - prints args
- * @ac: takes in width of grid
- * @av: height of grid
- * Return: the args one line at a time
+ * _strlen - gets string length
+ * @str: string
+ * Return: length (int)
+ */
+
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * argstostr - concatenates all the arguments to a program
+ * @ac: argc - number of arguments
+ * @av: argv - array of arguments
+ * Return: pointe to new string
  */
 
 char *argstostr(int ac, char **av)
 {
-	int *str;
-	int count = 0, a = 0, b = 0, c = 0;
+	int len, i, j, k;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	while (a < ac)
+
+	len = 0;
+	for (i = 0; i < ac; i++)
 	{
-		b = 0;
-		while (av[a][b] != '\0')
-		{
-			count++;
-			b++;
-		}
-		a++;
+		len += _strlen(av[i]) + 1;
 	}
-	count = count + ac + 1;
-	str = malloc(sizeof(char) * count);
+
+	str = malloc(sizeof(char) * len + 1);
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	for (a = 0; a < ac; a++)
+
+	k = 0;
+	for (i = 0; i < ac; i++)
 	{
-		for (b = 0; av[a][b] != '\0'; b++)
+		for (j = 0; j < _strlen(av[i]); j++)
 		{
-			str[c] = av[a][b];
-			c++;
+			str[k] = av[i][j];
+			k++;
 		}
-		str[c] = '\n';
-		c++;
+		str[k] = '\n';
+		k++;
 	}
+	str[k] = '\0';
 	return (str);
 }
