877e4fa6f81d5a4add4c93d53a8e03c43b386de5
korantengtony2 <korantengtony2@gmail.com>
2023-01-31 06:25:46 -0800

    task1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *current = NULL;
 	unsigned int i = 0;
+
 	if (*head == NULL)
 		return (-1);
 	if (index == 0)
@@ -19,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-						
+
 	while (i < index - 1)
 	{
 		if (!temp || !(temp->next))
